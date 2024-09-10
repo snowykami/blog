81ff1f3d708e9de833013c5eac0917e4b521a7f2
snowykami <snowykami@outlook.com>
2024-09-10 11:00:29 +0000

    Deploying to pages from @ snowykami/blog@1a5e70507d2871c78a20a075b626fa726b084b72 🚀

---
+++ assets/chunks/@localSearchIndexen.WQ89DRQB.js
@@ -0,0 +1,1 @@
+const e='{"documentCount":1,"nextId":1,"documentIds":{"0":"/en/#blog"},"fieldIds":{"title":0,"titles":1,"text":2},"fieldLength":{"0":[1,1,4]},"averageFieldLength":[1,1,4],"storedFields":{"0":{"title":"blog","titles":[]}},"dirtCount":0,"index":[["s",{"2":{"0":1}}],["snowykami",{"2":{"0":1}}],["blog",{"0":{"0":1},"2":{"0":1}}]],"serializationVersion":2}';export{e as default};
